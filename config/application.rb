28ffc4f27c4b836ec099789a6148e375487c8674
tomoki <tomoki@tomokis-MacBook-Pro.local>
2023-03-25 17:39:46 +0900

    helperファイルを自動生成しないよう設定

--- config/application.rb
+++ config/application.rb
@@ -9,11 +9,12 @@ module CardCollection
     config.load_defaults 7.0
 
     config.generators do |g|
-     g.test_framework :rspec,
-        fixtures: false,
-        view_specs: false,
-        helper_specs: false,
-        routing_specs: false
+    g.helper false
+    g.test_framework :rspec,
+      fixtures: false,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false
     end
   end
 end
