08c42057ef97c8db75a279b0cde190e73c501ede
tomoki <tomoki@tomokis-MacBook-Pro.local>
2023-04-10 22:26:50 +0900

    CardListのモデルスペックを実装

--- spec/models/card_list_spec.rb
+++ spec/models/card_list_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe CardList, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーション' do
+    let(:user) { create(:user) }
+    let!(:card_list) { create(:card_list, user: user) }
+
+    context 'titleが存在する場合' do
+      it '登録できること' do
+        expect(card_list).to be_valid
+      end
+    end
+
+    context 'titleが存在しない場合' do
+      it '登録できないこと' do
+        card_list.title = nil
+        expect(card_list).not_to be_valid
+      end
+    end
+  end
 end
