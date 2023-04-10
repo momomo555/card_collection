f86bfdfe801d65c5652b14af89216ad616b0018c
tomoki <tomoki@tomokis-MacBook-Pro.local>
2023-04-10 19:43:48 +0900

    Userのモデルスペックを実装

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーション' do
+    let!(:user) { create(:user) }
+
+    context 'email、passwordとpassword_confirmationが存在する場合' do
+      it '登録できること' do
+        expect(user).to be_valid
+      end
+    end
+
+    context 'emailが存在しない場合' do
+      it '登録できないこと' do
+        user.email = nil
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'passwordが存在しない場合' do
+      it '登録できないこと' do
+        user.password = nil
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'passwordとpassword_confirmationが一致しない場合' do
+      it '登録できないこと' do
+        user.password_confirmation = 'pass'
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'passwordが6文字未満の場合' do
+      it '登録できないこと' do
+        user.password = 'abcde'
+        user.password_confirmation = 'abcde'
+        expect(user).not_to be_valid
+      end
+    end
+  end
 end
