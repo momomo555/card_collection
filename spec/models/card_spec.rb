7d7ba89283ca76bf84d0152b8c0652e028bf5766
tomoki <tomoki@tomokis-MacBook-Pro.local>
2023-04-10 22:53:57 +0900

    Cardのモデルスペックを実装

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーション' do
+    let(:user) { create(:user) }
+    let(:card_list) { create(:card_list, user: user) }
+    let!(:card) { create(:card, card_list: card_list) }
+
+    context 'nameが存在する場合' do
+      it '登録できること' do
+        expect(card).to be_valid
+      end
+    end
+
+    context 'nameが存在しない場合' do
+      it '登録できないこと' do
+        card.name = nil
+        expect(card).not_to be_valid
+      end
+    end
+  end
 end
