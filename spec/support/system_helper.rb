ca045255ab0fa73008e0ace7a6f579bdfefd3738
tomoki <tomoki@tomokis-MacBook-Pro.local>
2023-04-12 11:43:38 +0900

    Userのシステムスペックを実装

--- spec/support/system_helper.rb
+++ spec/support/system_helper.rb
@@ -1,6 +1,6 @@
 module SystemHelper
   def login_as(user)
-    visit login_path
+    visit new_user_session_path
     fill_in 'Eメール', with: user.email
     fill_in 'パスワード', with: 'password'
     click_button 'ログイン'
